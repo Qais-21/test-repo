a950ea15c193320baa9e9014b1fd0eb89e5d9886
Qais-21 <135427485+Qais-21@users.noreply.github.com>
2023-06-03 14:24:57 +0300

    c2

---
+++ Ultrasonic-main.py
@@ -0,0 +1,14 @@
+from microbit import *
+from ultrasonic import Ultrasonic
+ultrasonic_sensor= Ultrasonic()
+while True:
+    distance_value = ultrasonic_sensor.measure_distance_cm()
+    if distance_value <16:
+        display.scroll(str(int(distance_value)))
+        sleep(2000)
+        display.scroll("Danger")
+    elif distance_value > 15:
+        display.scroll(str(int(distance_value)))
+        sleep(2000)
+        
+        display.scroll("add")
